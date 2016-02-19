2f7bdf8a41891e5fb95b37c962672d532b8ab89e
Leah Holyoak <leahholyoak@gmail.com>
2016-02-19 01:42:56 +0000

    Added index and update functionality to articles

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   root 'pages#home'
-  get 'pages/home', to: 'pages#home'
   get 'pages/about', to: 'pages#about'
+  resources :articles
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
