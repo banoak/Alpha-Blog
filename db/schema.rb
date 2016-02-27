b3a135f6646b0842a15d93f729637d21e8ad51dd
Leah Holyoak <holyoak93@gmail.com>
2016-02-27 15:40:55 -0700

    Pepare for deploy

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160226195658) do
 
   create_table "articles", force: :cascade do |t|
@@ -19,4 +18,6 @@ ActiveRecord::Schema.define(version: 20160226195658) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
 end
